3514aafda973d131f8e24f2aeb6d7979eb96bbe9
Bartosz Stachowiak <sbartekt@op.pl>
2022-12-15 17:29:48 +0100

    update: make dashboard runnable with flask

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from dashboard.app import app
+import os
 
+from dashboard.app import app as dash_app
+
+app = dash_app.server  # expose the underlying Flask server object
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    dash_app.run_server(debug=os.environ.get("DEBUG_MODE", True))
