d97f55ca9ef582278ca07bd8e9f620aac5bfdf8a
Bartosz Stachowiak <sbartekt@op.pl>
2022-12-18 14:02:12 +0100

    feat: remove GlobalState from the app, use Session storage on clientside for storing data

--- dashboard/dynamic/callbacks.py
+++ dashboard/dynamic/callbacks.py
@@ -3,22 +3,21 @@ Defines and registers callbacks for the dashboard.
 """
 
 import typing
-import functools
+
+import pandas as pd
 
 from dash import html, Dash, dcc
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State
 
-from src.data.parse_data import combine_assays
+from src.data.parse_data import combine_assays, get_projections
 
 from .tables import table_from_df
 from .figures import scatterplot_from_df, make_projection_plot
-from ..parse import parse_contents
-from ..state import GlobalState
+from ..parse import parse_contents, get_crucial_column_names
 
 
 def on_data_upload(
-    global_state: GlobalState,
     contents: typing.Iterable,
     names: typing.Iterable[str],
 ) -> tuple[html.Div, html.Div, list[str], str, list[str], str, list[str], str]:
@@ -28,7 +27,6 @@ def on_data_upload(
     Updates the description table, preview table, and dropdowns.
     Only accepts two or more files.
 
-    :param global_state: global state of the dashboard
     :param contents: list of uploaded file contents
     :param names: list of uploaded file names
     :raises PreventUpdate: if no files were uploaded
@@ -51,43 +49,51 @@ def on_data_upload(
         ]
     except AttributeError:
         pass
-    global_state.set_dataframe(processed_dataframe)
 
+    crucial_columns = get_crucial_column_names(processed_dataframe.columns)
+
+    strict_df = processed_dataframe[["CMPD ID"] + crucial_columns]
+    strict_summary_df = (
+        strict_df[crucial_columns].describe().round(3).T.reset_index(level=0)
+    )
     description_table = table_from_df(
-        global_state.strict_summary_df,
+        strict_summary_df,
         "description-table",
     )
-    preview_table = table_from_df(global_state.df, "preview-table")
+    projection_df = get_projections(strict_df, get_3d=False)
+
+    preview_table = table_from_df(projection_df, "preview-table")
 
     return (
         description_table,
         preview_table,
-        global_state.crucial_columns,  # x-axis dropdown options
-        global_state.crucial_columns[0],  # x-axis dropdown value
-        global_state.crucial_columns,  # y-axis dropdown options
-        global_state.crucial_columns[0],  # y-axis dropdown value
-        global_state.crucial_columns,  # colormap-feature dropdown options
-        global_state.crucial_columns[0],  # colormap-feature dropdown value
+        crucial_columns,  # x-axis dropdown options
+        crucial_columns[0],  # x-axis dropdown value
+        crucial_columns,  # y-axis dropdown options
+        crucial_columns[0],  # y-axis dropdown value
+        crucial_columns,  # colormap-feature dropdown options
+        crucial_columns[0],  # colormap-feature dropdown value
+        projection_df.to_json(date_format="iso", orient="split"),
     )
 
 
 def on_projection_plot_selection(
-    global_state: GlobalState, relayoutData: dict, projection_type: str
+    relayoutData: dict, projection_type: str, projection_data: str
 ) -> dict:
     """
     Callback on projection selection (i.e. zooming in).
     Limits the preview table records to the selected range.
 
-    :param global_state: global state of the application containing the dataframe
     :param relayoutData: dictionary containing the new range
     :param projection_type: type of the projection
+    :param projection_data: jsonified dataframe with projection data
     :raises PreventUpdate: if no relayoutData or projection_type is provided
     :return: restricted dataframe as a dictionary
     """
     if not relayoutData or not projection_type:
         raise PreventUpdate
 
-    df = global_state.projections_df
+    df = pd.read_json(projection_data, orient="split")
     if "xaxis.range[0]" in relayoutData:
         x_min = relayoutData["xaxis.range[0]"]
         x_max = relayoutData["xaxis.range[1]"]
@@ -95,25 +101,23 @@ def on_projection_plot_selection(
         y_min = relayoutData["yaxis.range[0]"]
         y_max = relayoutData["yaxis.range[1]"]
         df = df[df[f"{projection_type}_Y"].between(y_min, y_max)]
-    return global_state.df[global_state.df["CMPD ID"].isin(df["CMPD ID"])].to_dict(
-        "records"
-    )
+    return df[df["CMPD ID"].isin(df["CMPD ID"])].to_dict("records")
 
 
-def on_axis_change(global_state: GlobalState, x_attr: str, y_attr: str) -> dcc.Graph:
+def on_axis_change(x_attr: str, y_attr: str, projection_data: str) -> dcc.Graph:
     """
     Callback on axis dropdown change.
     Updates the basic plot.
 
-    :param global_state: global state of the application containing the dataframe
     :param x_attr: new x axis attribute
     :param y_attr: new y axis attribute
+    :param projection_data: jsonified dataframe with projection data
     :return: html Div containing the basic plot
     """
     if not x_attr or not y_attr:
         raise PreventUpdate
     return scatterplot_from_df(
-        global_state.strict_df,
+        pd.read_json(projection_data, orient="split"),
         x_attr,
         y_attr,
         "Compounds experimens results",
@@ -122,26 +126,26 @@ def on_axis_change(global_state: GlobalState, x_attr: str, y_attr: str) -> dcc.G
 
 
 def on_projection_settings_change(
-    global_state: GlobalState, projection_type: str, colormap_attr: str
+    projection_type: str, colormap_attr: str, projection_data: str
 ) -> dcc.Graph:
     """
     Callback on projection settings change.
     Updates the projection plot.
 
-    :param global_state: global state of the application containing the dataframe
     :param projection_type: type of the projection to be visualized
     :param colormap_attr: column to be used for coloring the points
+    :param global_state: global state of the application contaiing the dataframe
     :raises PreventUpdate: if no projection_type or colormap_attr is provided
     :return: dcc Graph object representing the projection plot
     """
     if not projection_type or not colormap_attr:
         raise PreventUpdate
     return make_projection_plot(
-        global_state.projections_df, colormap_attr, projection_type
+        pd.read_json(projection_data, orient="split"), colormap_attr, projection_type
     )
 
 
-def register_callbacks(app: Dash, global_state: GlobalState) -> None:
+def register_callbacks(app: Dash) -> None:
     """
     Registers application callbacks.
 
@@ -158,26 +162,32 @@ def register_callbacks(app: Dash, global_state: GlobalState) -> None:
             Output("y-axis-dropdown", "value"),
             Output("colormap-attribute-dropdown", "options"),
             Output("colormap-attribute-dropdown", "value"),
+            Output("data-holder", "data"),
         ],
         Input("upload-data", "contents"),
         State("upload-data", "filename"),
-    )(functools.partial(on_data_upload, global_state))
+    )(on_data_upload)
     app.callback(
         Output("preview-table", "data"),
         Input("projection-plot", "relayoutData"),
-        State("projection-type-dropdown", "value"),
-    )(functools.partial(on_projection_plot_selection, global_state))
+        [
+            State("projection-type-dropdown", "value"),
+            State("data-holder", "data"),
+        ],
+    )(on_projection_plot_selection)
     app.callback(
         Output("basic-plot-slot", "children"),
         [
             Input("x-axis-dropdown", "value"),
             Input("y-axis-dropdown", "value"),
         ],
-    )(functools.partial(on_axis_change, global_state))
+        State("data-holder", "data"),
+    )(on_axis_change)
     app.callback(
         Output("projection-plot-slot", "children"),
         [
             Input("projection-type-dropdown", "value"),
             Input("colormap-attribute-dropdown", "value"),
         ],
-    )(functools.partial(on_projection_settings_change, global_state))
+        State("data-holder", "data"),
+    )(on_projection_settings_change)
