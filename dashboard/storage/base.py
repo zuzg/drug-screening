f4799759aec6d4a2d477012559ea555d3909b4fe
Bartosz Stachowiak <sbartekt@op.pl>
2023-04-19 15:21:05 +0200

    chore: add missing newlines, remove extra newlines

--- dashboard/storage/base.py
+++ dashboard/storage/base.py
@@ -1,12 +1,11 @@
 import abc
 
+
 class FileStorage(abc.ABC):
     @abc.abstractmethod
     def read_file(self, name: str) -> bytes:
         ...
-    
+
     @abc.abstractmethod
     def save_file(self, name: str, content: bytes) -> None:
         ...
-
-    
