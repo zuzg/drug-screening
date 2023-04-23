eff6f8f2872a36b264086bc4871ae55f7d540fdf
azywot <agata.zywot@gmail.com>
2023-04-23 21:59:49 +0200

    add suggestions

--- src/data/file_preprocessing/echo_files_parser.py
+++ src/data/file_preprocessing/echo_files_parser.py
@@ -13,7 +13,7 @@ class EchoFilesParser:
         self.echo_df = pd.DataFrame()
         self.exceptions_df = pd.DataFrame()
 
-    def find_marker_rows(self, file:str, markers:tuple[str]) -> list[int]:
+    def find_marker_rows(self, file: str, markers: tuple[str]) -> list[int]:
         """
         Finds the row numbers of marker lines in a file.
 
@@ -38,6 +38,7 @@ class EchoFilesParser:
         if not(all(file.endswith('.csv') for file in self.echo_files)):
             raise ValueError(f"Expected files to be of '*.csv' type, provided: {self.echo_files}")
 
+        exception_dfs, echo_dfs = [], []
         for filename in self.echo_files: 
             markers = self.find_marker_rows(filename, ('[EXCEPTIONS]', '[DETAILS]'))
 
@@ -50,17 +51,19 @@ class EchoFilesParser:
                 echo_df = pd.read_csv(filename, skiprows=markers[0]+1)
 
             echo_df = echo_df[~echo_df[echo_df.columns[0]].str.lower().str.startswith('instrument')]
+            exception_dfs.append(exceptions_df)
+            echo_dfs.append(echo_df)
 
-            self.exceptions_df = exceptions_df if self.exceptions_df.empty else pd.concat([self.exceptions_df, exceptions_df], ignore_index=True)
-            self.echo_df = echo_df if self.echo_df.empty else pd.concat([self.echo_df, echo_df], ignore_index=True)
+        self.exceptions_df = pd.concat(exception_dfs, ignore_index=True)
+        self.echo_df = pd.concat(echo_dfs, ignore_index=True)
 
         return self
     
     def link_bmg_files(self, 
                        bmg_files: list[str],
-                       bmg_columns: list[str] = ['Well', 'Value'],
-                       bmg_keys: list[str] = ['Plate_barcode', 'Well'],
-                       echo_keys: list[str] = ['Destination Plate Barcode','Destination Well']) -> EchoFilesParser:
+                       bmg_columns: list[str] = ('Well', 'Value'),
+                       bmg_keys: list[str] = ('Plate_barcode', 'Well'),
+                       echo_keys: list[str] = ('Destination Plate Barcode','Destination Well')) -> EchoFilesParser:
         """
         Links bmg files to the echo files.
 
@@ -68,20 +71,15 @@ class EchoFilesParser:
         :param echo_keys: list of echo keys to merge on
         :param bmg_keys: list of bmg keys to merge on       
         """
-        echo_bmg_linked_df = pd.DataFrame()
+        echo_bmg_linked_dfs = []
 
         for bmg_file in bmg_files:
             plate_barcode = bmg_file.split('.')[0].split('\\')[-1]
             bmg_df = pd.read_csv(bmg_file, sep='\t', names=bmg_columns)
             bmg_df[bmg_keys[0]]=plate_barcode
-            echo_bmg_linked_df = self.echo_df.merge(bmg_df, 
-                                              left_on=echo_keys, 
-                                              right_on=bmg_keys) if echo_bmg_linked_df.empty else pd.concat([echo_bmg_linked_df, 
-                                                                                                            self.echo_df.merge(bmg_df, 
-                                                                                                                                left_on=echo_keys, 
-                                                                                                                                right_on=bmg_keys)],
-                                                                                                            ignore_index=True)
-        self.echo_df = echo_bmg_linked_df.drop(columns=bmg_keys)
+
+            echo_bmg_linked_dfs.append(self.echo_df.merge(bmg_df, left_on=echo_keys, right_on=bmg_keys))
+        self.echo_df = pd.concat(echo_bmg_linked_dfs, ignore_index=True)
         return self
 
     def retain_columns(self, columns: list[str]) -> EchoFilesParser:
@@ -95,7 +93,7 @@ class EchoFilesParser:
         self.echo_df = self.echo_df[retain_echo]
 
         retain_exceptions = list(set(columns).intersection(self.exceptions_df.columns))
-        self.exceptions_df = self.exceptions_df[retain_exceptions]
+        self.exceptions_df = self.exceptions_df[retain_exceptions].sort_index(axis=1)
         return self
 
     def get_processed_echo_df(self) -> pd.DataFrame:
