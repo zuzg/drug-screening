ca567967ccd15197cb009063e1dd6f72d6374c04
Bartosz Stachowiak <sbartekt@op.pl>
2023-05-25 20:30:04 +0200

    feat: implement paginated heatmap plates viewer

--- dashboard/pages/primary_screening/callbacks.py
+++ dashboard/pages/primary_screening/callbacks.py
@@ -1,14 +1,21 @@
-from dash import html, no_update, callback, Output, Input, State
-
-from ..error import throwable
-from ...data.bmg_plate import parse_bmg_files
-from ...data.file_preprocessing.echo_files_parser import EchoFilesParser
 import io
 import base64
 import functools
 import uuid
 import numpy as np
-import pandas
+import pandas as pd
+import pyarrow as pa
+import plotly.graph_objects as go
+
+from dash import html, no_update, callback, Output, Input, State, callback_context
+
+from dashboard.data.bmg_plate import parse_bmg_files
+from dashboard.data.file_preprocessing.echo_files_parser import EchoFilesParser
+from dashboard.storage import FileStorage
+from dashboard.visualization.plots import visualize_multiple_plates
+
+
+# === STAGE 1 ===
 
 
 def upload_bmg_data(contents, names, last_modified, stored_uuid, file_storage):
@@ -49,6 +56,55 @@ def upload_bmg_data(contents, names, last_modified, stored_uuid, file_storage):
     )
 
 
+# === STAGE 2 ===
+
+DISPLAYED_PLATES = 9
+DIM = int(np.ceil(np.sqrt(DISPLAYED_PLATES)))
+
+
+def on_heatmap_controls_clikced(
+    n_clicks_prev: int,
+    n_clicks_next: int,
+    current_index: int,
+    max_index: int,
+) -> int:
+    triggered = callback_context.triggered[0]["prop_id"].split(".")[0]
+    if triggered == "heatmap-previous-btn":
+        return max(0, current_index - DISPLAYED_PLATES)
+    if triggered == "heatmap-next-btn" and current_index < max_index:
+        return current_index + DISPLAYED_PLATES
+    return no_update
+
+
+def on_outlier_purge_stage_entry(
+    current_stage: int,
+    heatmap_start_index: int,
+    stored_uuid: str,
+    file_storage: FileStorage,
+) -> tuple[go.Figure, int, str]:
+    if current_stage != 1:
+        return no_update
+    raw_bmg = file_storage.read_file(f"{stored_uuid}_bmg_df.pq")
+    bmg_df = pd.read_parquet(pa.BufferReader(raw_bmg))
+    raw_vals = file_storage.read_file(f"{stored_uuid}_bmg_val.npz")
+    bmg_vals = np.load(io.BytesIO(raw_vals))["arr_0"]
+    vis_bmg_df = bmg_df.iloc[
+        heatmap_start_index : heatmap_start_index + DISPLAYED_PLATES, :
+    ]
+    vis_bmg_vals = bmg_vals[
+        heatmap_start_index : heatmap_start_index + DISPLAYED_PLATES
+    ]
+    fig = visualize_multiple_plates(vis_bmg_df, vis_bmg_vals, DIM, DIM)
+    index_text = f"{heatmap_start_index + 1} - {heatmap_start_index + DISPLAYED_PLATES} / {bmg_vals.shape[0]}"
+    return fig, max(0, bmg_vals.shape[0] - DISPLAYED_PLATES), index_text
+
+
+# === STAGE 3 ===
+
+
+# === STAGE 4 ===
+
+
 def upload_echo_data(contents, names, last_modified, stored_uuid, file_storage):
     if contents is None:
         return no_update
@@ -84,6 +140,9 @@ def upload_echo_data(contents, names, last_modified, stored_uuid, file_storage):
     )
 
 
+# === STAGE 5 ===
+
+
 def register_callbacks(elements, file_storage):
     callback(
         [
@@ -95,6 +154,24 @@ def register_callbacks(elements, file_storage):
         Input("upload-bmg-data", "last_modified"),
         State("user-uuid", "data"),
     )(functools.partial(upload_bmg_data, file_storage=file_storage))
+
+    callback(
+        Output("heatmap-start-index", "data"),
+        Input("heatmap-previous-btn", "n_clicks"),
+        Input("heatmap-next-btn", "n_clicks"),
+        State("heatmap-start-index", "data"),
+        State("max-heatmap-index", "data"),
+    )(on_heatmap_controls_clikced)
+
+    callback(
+        Output("plates-heatmap-graph", "figure"),
+        Output("max-heatmap-index", "data"),
+        Output("heatmap-index-display", "children"),
+        Input(elements["STAGES_STORE"], "data"),
+        Input("heatmap-start-index", "data"),
+        State("user-uuid", "data"),
+    )(functools.partial(on_outlier_purge_stage_entry, file_storage=file_storage))
+
     callback(
         Output("echo-filenames", "children"),
         Input("upload-echo-data", "contents"),
--- dashboard/pages/primary_screening/stages/s2_outliers_purging.py
+++ dashboard/pages/primary_screening/stages/s2_outliers_purging.py
@@ -1,4 +1,4 @@
-from dash import html
+from dash import html, dcc
 
 OUTLIERS_PURGING_STAGE = html.Div(
     id="outliers_purging_stage",
@@ -8,5 +8,54 @@ OUTLIERS_PURGING_STAGE = html.Div(
             children=["Outliers Purging"],
             className="text-center",
         ),
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="col",
+                    children=[
+                        html.Div(
+                            children=[
+                                html.H2(
+                                    children=["Plates Heatmap"],
+                                    className="text-center",
+                                ),
+                                dcc.Graph(
+                                    id="plates-heatmap-graph",
+                                ),
+                                html.Div(
+                                    className="d-flex justify-content-center gap-2 mt-3",
+                                    children=[
+                                        html.Button(
+                                            id="heatmap-previous-btn",
+                                            children=["Previous"],
+                                            className="btn btn-primary fixed-width-100",
+                                        ),
+                                        html.Div(
+                                            id="heatmap-index-display",
+                                            children=["0/0"],
+                                            className="text-center my-auto fixed-width-150 text-muted",
+                                        ),
+                                        html.Button(
+                                            id="heatmap-next-btn",
+                                            children=["Next"],
+                                            className="btn btn-primary fixed-width-100",
+                                        ),
+                                    ],
+                                ),
+                            ]
+                        ),
+                    ],
+                    style={"border": "1px dashed red"},  # TODO: REMOVE
+                ),
+                html.Div(
+                    className="col",
+                    children="TEST2",
+                    style={"border": "1px dashed red"},  # TODO: REMOVE
+                ),
+            ],
+        ),
+        dcc.Store(id="heatmap-start-index", data=0),
+        dcc.Store(id="max-heatmap-index", data=0),
     ],
 )
