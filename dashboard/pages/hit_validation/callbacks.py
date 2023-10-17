9308bcbf900e1f32ac36cd6a37c05eede950059e
Bartosz Stachowiak <72276326+Tremirre@users.noreply.github.com>
2023-10-17 21:50:35 +0200

    closes 126 implement third process (#133)
    * remove .venv from git tracking
    
    * implement basic parsing
    
    * implement file parsing and graph creation
    
    * implement csv downloading
    
    * remove unneeded files
    
    ---------
    
    Co-authored-by: njytwf <bartosz.stachowiak@aptiv.com>

--- dashboard/pages/hit_validation/callbacks.py
+++ dashboard/pages/hit_validation/callbacks.py
@@ -2,27 +2,33 @@ import base64
 import io
 import uuid
 import functools
-from datetime import datetime
 import json
-
 import pandas as pd
 import pyarrow as pa
 
-from dash import Input, Output, State, callback, html, no_update, ALL, callback_context
+from datetime import datetime
+
+from dash import (
+    Input,
+    Output,
+    State,
+    callback,
+    html,
+    no_update,
+    ALL,
+    callback_context,
+    dcc,
+)
 
 from dashboard.storage import FileStorage
 from dashboard.data.determination import perform_hit_determination
 from dashboard.visualization.plots import plot_ic50
 
+SCREENING_FILENAME = "{0}_screening_df.pq"
+HIT_FILENAME = "{0}_hit_df.pq"
+
 
 # === STAGE 1 ===
-
-EXPECTED_COLUMNS = {
-    "CMPD ID",
-    # TODO: specify the expected columns
-}
-
-
 def on_file_upload(
     content: str | None,
     stored_uuid: str,
@@ -47,17 +53,39 @@ def on_file_upload(
         stored_uuid = str(uuid.uuid4())
 
     decoded = base64.b64decode(content.split(",")[1]).decode("utf-8")
-    screen_df = pd.read_csv(io.StringIO(decoded))
+    screen_df = pd.read_csv(io.StringIO(decoded), delimiter=";")
+    if screen_df.shape[1] == 1:
+        screen_df = pd.read_csv(io.StringIO(decoded), delimiter=",")
+    screen_df = screen_df.rename(str.upper, axis="columns")
+
+    rename_dict = {}
+    for column in screen_df.columns:
+        if column.startswith("CONCENTRATION"):
+            rename_dict[column] = "CONCENTRATION"
+        elif column.startswith("INHIBITION"):
+            rename_dict[column] = "VALUE"
+        elif column.startswith("ACTIVATION"):
+            rename_dict[column] = "VALUE"
+
+    screen_df = screen_df.rename(rename_dict, axis=1)
     column_set = set(screen_df.columns)
 
-    if missing_columns := (EXPECTED_COLUMNS - column_set):
-        missing_message = ", ".join(sorted(missing_columns))
+    missing = []
+    if "EOS" not in column_set:
+        missing.append("column EOS")
+    if "CONCENTRATION" not in column_set:
+        missing.append("column starting with 'concentration'")
+    if "VALUE" not in column_set:
+        missing.append("column starting with 'activation' or 'inhibition'")
+
+    if missing:
+        missing_message = ", ".join(sorted(missing))
         return html.Div(
             children=[
                 html.I(className="fas fa-times-circle text-danger me-2"),
                 html.Span(
                     children=[
-                        f"File does not contain the expected columns: ",
+                        f"File does not contain the following: ",
                         html.Span(missing_message, className="fw-bold"),
                     ]
                 ),
@@ -65,28 +93,51 @@ def on_file_upload(
             className="text-danger",
         )
 
-    compounds_count = len(screen_df["CMPD ID"].unique())
-    saved_name = f"{stored_uuid}_screening.pq"
+    # screening df needs to be safed for plots
+    file_storage.save_file(
+        SCREENING_FILENAME.format(stored_uuid), screen_df.to_parquet(index=False)
+    )
+    compounds_count = len(screen_df["EOS"].unique())
+    saved_name = HIT_FILENAME.format(stored_uuid)
 
     # Placeholder for hit determination
     hit_determination_df = perform_hit_determination(
         screen_df, concentration_lower_bound, concentration_upper_bound
     )
+    unfit = hit_determination_df.EOS[hit_determination_df.ic50.isna()].tolist()
 
     file_storage.save_file(saved_name, hit_determination_df.to_parquet())
 
     return html.Div(
         children=[
-            html.I(className="fas fa-check-circle text-success me-2"),
-            html.Span(
+            html.Div(
                 children=[
-                    f"File uploaded successfully. Found ",
-                    html.Span(compounds_count, className="fw-bold"),
-                    " compounds.",
-                ]
+                    html.I(className="fas fa-check-circle me-2"),
+                    html.Span(
+                        children=[
+                            f"File uploaded successfully. Found ",
+                            html.Span(compounds_count, className="fw-bold"),
+                            " compounds.",
+                        ],
+                    ),
+                ],
+                className="text-success",
+            ),
+            html.Div(
+                children=[
+                    html.I(className="fas fa-exclamation-circle me-2"),
+                    html.Span(
+                        children=[
+                            f"Found ",
+                            html.Span(len(unfit), className="fw-bold"),
+                            " compounds that failed curve fit: ",
+                            html.Span(", ".join(unfit), className="fw-bold"),
+                        ],
+                    ),
+                ],
+                className="text-warning",
             ),
         ],
-        className="text-success",
     )
 
 
@@ -140,12 +191,11 @@ def on_hit_browser_stage_entry(
     if current_stage != 1:
         return no_update
 
-    load_name = f"{stored_uuid}_screening.pq"
     hit_determination_df = pd.read_parquet(
-        pa.BufferReader(file_storage.read_file(load_name))
+        pa.BufferReader(file_storage.read_file(HIT_FILENAME.format(stored_uuid)))
     )
 
-    compounds_list = sorted(hit_determination_df["CMPD ID"].unique().tolist())
+    compounds_list = sorted(hit_determination_df["EOS"].unique().tolist())
     return [
         html.Button(
             compound,
@@ -166,33 +216,65 @@ def on_compound_button_click(n_clicks: int, compound_id: str) -> str:
     return callback_context.triggered_id["index"]
 
 
+activity_icons = {
+    "active": html.I(className="fas fa-check-circle text-success"),
+    "inactive": html.I(className="fas fa-times-circle text-danger"),
+    "inconclusive": html.I(className="fas fa-question-circle text-warning"),
+}
+
+
 def on_selected_compound_changed(
     selected_compound: str,
     stored_uuid: str,
     file_storage: FileStorage,
 ) -> html.Div:
-    load_name = f"{stored_uuid}_screening.pq"
+    """
+    Callback for selected compound change. It loads the data from the storage and
+    returns the data for the compound.
+
+    :param selected_compound: selected compound
+    :param stored_uuid: session uuid
+    :param file_storage: file storage
+    :return: data for the compound
+    """
+    hit_load_name = HIT_FILENAME.format(stored_uuid)
     hit_determination_df = pd.read_parquet(
-        pa.BufferReader(file_storage.read_file(load_name))
+        pa.BufferReader(file_storage.read_file(hit_load_name))
     )
+    screening_load_name = SCREENING_FILENAME.format(stored_uuid)
+    screening_data = pd.read_parquet(
+        pa.BufferReader(file_storage.read_file(screening_load_name))
+    ).loc[lambda df: df["EOS"] == selected_compound]
+    concentrations = screening_data["CONCENTRATION"].to_numpy()
+    values = screening_data["VALUE"].to_numpy()
+
     entry = (
-        hit_determination_df[hit_determination_df["CMPD ID"] == selected_compound]
+        hit_determination_df[hit_determination_df["EOS"] == selected_compound]
         .iloc[0]
         .to_dict()
     )
     # TODO: replace with real data
-    graph = plot_ic50(entry)
+    graph = plot_ic50(entry, concentrations, values)
+
     result = {
-        "id": "Compound " + entry["CMPD ID"],
-        "min-modulation": 0,
-        "max-modulation": 100,
-        "ic50": 0,
-        "curve-slope": 0,
-        "r2": 0,
-        "is-active": html.I(className="fas fa-times-circle text-danger"),
+        "id": entry["EOS"],
+        "min-modulation": round(entry["min_value"], 5),
+        "max-modulation": round(entry["max_value"], 5),
+        "ic50": round(entry["ic50"], 5),
+        "curve-slope": round(entry["slope"], 5),
+        "r2": round(entry["r2"] * 100, 5),
+        "is-active": html.Span(
+            children=[
+                activity_icons[entry["activity_final"]],
+                html.Span(entry["activity_final"].upper(), className="ms-1"),
+            ]
+        ),
         "graph": graph,
     }
     return tuple(result.values())
+
+
+# === STAGE 3 ===
 
 
 def on_json_generate_button_click(
@@ -203,6 +285,28 @@ def on_json_generate_button_click(
     filename = f"hit_validation_settings_{datetime.now().strftime('%Y-%m-%d')}.json"
     json_object = json.dumps(correlation_plots_report, indent=4)
     return dict(content=json_object, filename=filename)
+
+
+def on_download_summary_csv_button_click(
+    n_clicks,
+    stored_uuid: str,
+    file_storage: FileStorage,
+):
+    """
+    Callback for download summary csv button click. It loads the data from the storage
+    and returns the data for the compound.
+
+    :param n_clicks: number of clicks
+    :param stored_uuid: session uuid
+    :param file_storage: file storage
+    :return: hit determination data in csv format
+    """
+    filename = f"hit_validation_summary_{datetime.now().strftime('%Y-%m-%d')}.csv"
+    hit_df = pd.read_parquet(
+        pa.BufferReader(file_storage.read_file(HIT_FILENAME.format(stored_uuid)))
+    )
+
+    return dcc.send_data_frame(hit_df.to_csv, filename)
 
 
 def register_callbacks(elements, file_storage: FileStorage):
@@ -256,3 +360,14 @@ def register_callbacks(elements, file_storage: FileStorage):
         State("report-data-hit-validation-input", "data"),
         prevent_initial_call=True,
     )(functools.partial(on_json_generate_button_click, file_storage=file_storage))
+
+    callback(
+        Output("download-csv-summary-hit-validation", "data"),
+        Input("download-csv-summary-hit-validation-button", "n_clicks"),
+        State("user-uuid", "data"),
+        prevent_initial_call=True,
+    )(
+        functools.partial(
+            on_download_summary_csv_button_click, file_storage=file_storage
+        )
+    )
