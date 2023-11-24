9a2152d853ec040652b6225a6d8b67f25024eff1
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2023-11-24 14:28:11 +0100

    Add ML experiment setup (#176)
    * Add ml experiment setup
    
    * Add feature selection, hp tuning and docs

---
+++ scripts/ml/options.py
@@ -0,0 +1,22 @@
+import argparse
+
+from config import MLTrainingConfig
+from consts import DATA_FOLDER_NAME
+
+
+def parse_args() -> MLTrainingConfig:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dataset_name", type=str, default="LD50_Zhu")
+    parser.add_argument("--balance_dataset", type=bool, default=True)
+    parser.add_argument("--hp_tuning", type=bool, default=True)
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default=DATA_FOLDER_NAME,
+    )
+
+    args = parser.parse_args()
+    cfg = MLTrainingConfig(**vars(args))
+    cfg.log_self()
+    return cfg
