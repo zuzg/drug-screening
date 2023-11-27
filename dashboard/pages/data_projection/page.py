753404f49327b45ac9a1077530989ec5307aad65
azywot <agata.zywot@gmail.com>
2023-11-27 20:04:00 +0100

    fix responsiveness

--- dashboard/pages/data_projection/page.py
+++ dashboard/pages/data_projection/page.py
@@ -31,13 +31,12 @@ CARDS_DATA = [
     },
 ]
 
-children = [html.Div(), None, None, html.Div()]
-children[1:3] = [make_card_component(**card_data) for card_data in CARDS_DATA]
+children = [make_card_component(**card_data) for card_data in CARDS_DATA]
 
 pb = PageBuilder(name=NAME)
 pb.extend_layout(
     layout=html.Main(
-        className="h-100 flex-grow-1 grid-1-1-1-1 gap-3 mx-auto my-5 container-xxl",
+        className="h-100 flex-grow-1 grid-1-1-projections gap-3 mx-auto my-5 container-xxl",
         children=children,
     )
 )
